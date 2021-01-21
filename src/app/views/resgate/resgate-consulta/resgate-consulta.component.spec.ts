a7a7b6fc53711393d3f22ead26b97e2f7591cad4
Marcos Paulo <marcospsilva222@hotmail.com>
2021-01-21 11:39:51 -0300

    teste componente

--- src/app/views/resgate/resgate-consulta/resgate-consulta.component.spec.ts
+++ src/app/views/resgate/resgate-consulta/resgate-consulta.component.spec.ts
@@ -1,13 +1,26 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ResgateConsultaComponent } from './resgate-consulta.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {RouterTestingModule} from '@angular/router/testing';
+import { ResgateDetalheComponent } from '../resgate-detalhe/resgate-detalhe.component';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import { Investimentos } from '../../../core/template/modal/investimentos';
+
 
 describe('ResgateConsultaComponent', () => {
   let component: ResgateConsultaComponent;
   let fixture: ComponentFixture<ResgateConsultaComponent>;
 
+  let investimento: Investimentos = new Investimentos();
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+  ReactiveFormsModule,
+        FormsModule,
+        HttpClientTestingModule,
+        RouterTestingModule.withRoutes([ { path: 'resgate-detalhe', component: ResgateDetalheComponent}]),
+      ],
       declarations: [ ResgateConsultaComponent ]
     })
     .compileComponents();
@@ -19,7 +32,15 @@ describe('ResgateConsultaComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('Deve testar o Componente', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Deve testar o metodo detalhar()', () => {
+    expect(component.detalhar(investimento)).toBe();
+  });
+
+  it('Deve testar o metodo listarTodosInvestimentos()', () => {
+    expect(component.listarTodosInvestimentos()).toBe();
   });
 });
