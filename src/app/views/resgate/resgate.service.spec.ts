4af853609ff906bd46cbe975ea73d72718801381
Marcos Paulo <marcospsilva222@hotmail.com>
2021-01-21 11:31:35 -0300

    teste classe servico

--- src/app/views/resgate/resgate.service.spec.ts
+++ src/app/views/resgate/resgate.service.spec.ts
@@ -1,16 +1,36 @@
 import { TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+
+
 
 import { ResgateService } from './resgate.service';
+import { environment } from 'src/environments/environment';
+import { RetornoDados } from 'src/app/core/template/modal/retorno';
+import { Investimentos } from 'src/app/core/template/modal/investimentos';
 
-describe('ResgateService', () => {
-  let service: ResgateService;
+describe('ResgateService', (): void => {
+  let service: ResgateService,  httpController: HttpTestingController;
+ 
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        BrowserAnimationsModule,
+        NoopAnimationsModule
+      ],
+      providers: [ResgateService]
+    });
     service = TestBed.inject(ResgateService);
+    httpController = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
+  it('Deve testar classe servico', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('Deve testar o metodo ', () => {
+    expect(service.getInvestimento()).toBeTruthy();
   });
 });
